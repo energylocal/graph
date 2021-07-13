45ce902a3d3dd150fd6e4a689281b34311ee031d
TrystanLea <trystan.lea@gmail.com>
2021-07-13 20:54:39 +0000

    hide menu

--- graph_menu.php
+++ graph_menu.php
@@ -1,12 +1,13 @@
 <?php
 global $session;
 // Initial graph menu item placement
+if ($session['admin']) {
 $menu["setup"]["l2"]['graph'] = array(
     "name"=>"Graphs",
     "href"=>"graph",
     "order"=>3, 
     "icon"=>"show_chart"
 );
-
+}
 // Full level3 sidebar is added via javascript in graph.js
 

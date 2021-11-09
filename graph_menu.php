7bad8a055e73d93abc2552e08e13ccc377f30973
TrystanLea <trystan.lea@gmail.com>
2021-11-09 10:19:38 +0000

    hide menu

--- graph_menu.php
+++ graph_menu.php
@@ -1,13 +1,13 @@
 <?php
-global $session;
+global $session, $route;
 // Initial graph menu item placement
-if ($session['admin']) {
-$menu["setup"]["l2"]['graph'] = array(
-    "name"=>"Graphs",
-    "href"=>"graph",
-    "order"=>3, 
-    "icon"=>"show_chart"
-);
+if ($session['admin'] || $route->controller=="graph") {
+    $menu["setup"]["l2"]['graph'] = array(
+        "name"=>"Graphs",
+        "href"=>"graph",
+        "order"=>3, 
+        "icon"=>"show_chart"
+    );
 }
 // Full level3 sidebar is added via javascript in graph.js
 
